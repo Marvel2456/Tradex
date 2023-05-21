e4763efa1bf075634ffa11bd446cbbd7d13dc4a6
Marvel2456 <marvellousidemudia@gmail.com>
2023-05-21 21:28:30 +0100

    wrote models for the accounts app

--- Trading/accounts/admin.py
+++ Trading/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Trader, Trade
 # Register your models here.
+
+
+admin.site.register(Trader)
+admin.site.register(Trade)
--- Trading/accounts/views.py
+++ Trading/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Trade, Trader
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
